ff48eb0630dee5cd058ceadea6ea18d2aeb3a47a
Mitchell Hollander <MitchellSHollander@gmail.com>
2021-12-06 00:04:32 -0600

    added more queries

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -83,7 +83,6 @@ export const FEATURED_QUERY = `
   }
 `;
 
-// variables: {page: 1, type: "ANIME", sort: "SEARCH_MATCH", search: "to love ru"}
 export const SEARCH_QUERY = `
   query ($page: Int = 1, $id: Int, $type: MediaType, $isAdult: Boolean = false, $search: String, $format: [MediaFormat], $status: MediaStatus, $countryOfOrigin: CountryCode, $source: MediaSource, $season: MediaSeason, $seasonYear: Int, $year: String, $onList: Boolean, $yearLesser: FuzzyDateInt, $yearGreater: FuzzyDateInt, $episodeLesser: Int, $episodeGreater: Int, $durationLesser: Int, $durationGreater: Int, $chapterLesser: Int, $chapterGreater: Int, $volumeLesser: Int, $volumeGreater: Int, $licensedBy: [String], $genres: [String], $excludedGenres: [String], $tags: [String], $excludedTags: [String], $minimumTagRank: Int, $sort: [MediaSort] = [POPULARITY_DESC, SCORE_DESC]) {
     Page(page: $page, perPage: 20) {
@@ -157,6 +156,322 @@ export const DETAILS_QUERY = `
       id
       title {
         userPreferred
+        english
+      }
+      coverImage {
+        extraLarge
+        large
+      }
+      bannerImage
+      description
+    }
+  }
+`;
+
+// Unique to AniList queries.
+
+export const ANILIST_USER_ACTIVITY_QUERY = `
+  query ($id: Int, $type: ActivityType, $page: Int = 1) {
+    Page(page: $page, perPage: 25) {
+      pageInfo {
+        total
+        perPage
+        currentPage
+        lastPage
+        hasNextPage
+      }
+      activities(userId: $id, type: $type, sort: ID_DESC) {
+        ... on ListActivity {
+          id
+          type
+          replyCount
+          status
+          progress
+          isLocked
+          isSubscribed
+          isLiked
+          likeCount
+          createdAt
+          user {
+            id
+            name
+            avatar {
+              large
+            }
+          }
+          media {
+            id
+            type
+            status(version: 2)
+            isAdult
+            bannerImage
+            title {
+              userPreferred
+            }
+            coverImage {
+              large
+            }
+          }
+        }
+        ... on TextActivity {
+          id
+          type
+          text
+          replyCount
+          isLocked
+          isSubscribed
+          isLiked
+          likeCount
+          createdAt
+          user {
+            id
+            name
+            avatar {
+              large
+            }
+          }
+        }
+        ... on MessageActivity {
+          id
+          type
+          message
+          replyCount
+          isPrivate
+          isLocked
+          isSubscribed
+          isLiked
+          likeCount
+          createdAt
+          user: recipient {
+            id
+          }
+          messenger {
+            id
+            name
+            donatorTier
+            donatorBadge
+            moderatorRoles
+            avatar {
+              large
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const ANILIST_USER_DETAILS_QUERY = `
+  query ($id: Int, $name: String) {
+    User(id: $id, name: $name) {
+      id
+      name
+      previousNames {
+        name
+        updatedAt
+      }
+      avatar {
+        large
+      }
+      bannerImage
+      about
+      isFollowing
+      isFollower
+      donatorTier
+      donatorBadge
+      createdAt
+      moderatorRoles
+      isBlocked
+      bans
+      options {
+        profileColor
+      }
+      mediaListOptions {
+        scoreFormat
+      }
+      statistics {
+        anime {
+          count
+          meanScore
+          standardDeviation
+          minutesWatched
+          episodesWatched
+          genrePreview: genres(limit: 10, sort: COUNT_DESC) {
+            genre
+            count
+          }
+        }
+        manga {
+          count
+          meanScore
+          standardDeviation
+          chaptersRead
+          volumesRead
+          genrePreview: genres(limit: 10, sort: COUNT_DESC) {
+            genre
+            count
+          }
+        }
+      }
+      stats {
+        activityHistory {
+          date
+          amount
+          level
+        }
+      }
+      favourites {
+        anime {
+          edges {
+            favouriteOrder
+            node {
+              id
+              type
+              status(version: 2)
+              format
+              isAdult
+              bannerImage
+              title {
+                userPreferred
+              }
+              coverImage {
+                large
+              }
+              startDate {
+                year
+              }
+            }
+          }
+        }
+        manga {
+          edges {
+            favouriteOrder
+            node {
+              id
+              type
+              status(version: 2)
+              format
+              isAdult
+              bannerImage
+              title {
+                userPreferred
+              }
+              coverImage {
+                large
+              }
+              startDate {
+                year
+              }
+            }
+          }
+        }
+        characters {
+          edges {
+            favouriteOrder
+            node {
+              id
+              name {
+                userPreferred
+              }
+              image {
+                large
+              }
+            }
+          }
+        }
+        staff {
+          edges {
+            favouriteOrder
+            node {
+              id
+              name {
+                userPreferred
+              }
+              image {
+                large
+              }
+            }
+          }
+        }
+        studios {
+          edges {
+            favouriteOrder
+            node {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export const ANILIST_USER_MEDIA_LIST_COLLECTION_QUERY = `
+  query ($userId: Int, $userName: String, $type: MediaType) {
+    MediaListCollection(userId: $userId, userName: $userName, type: $type) {
+      lists {
+        name
+        isCustomList
+        isCompletedList: isSplitCompletedList
+        entries {
+          ...mediaListEntry
+        }
+      }
+      user {
+        id
+        name
+        avatar {
+          large
+        }
+        mediaListOptions {
+          scoreFormat
+          rowOrder
+          animeList {
+            sectionOrder
+            customLists
+            splitCompletedSectionByFormat
+            theme
+          }
+          mangaList {
+            sectionOrder
+            customLists
+            splitCompletedSectionByFormat
+            theme
+          }
+        }
+      }
+    }
+  }
+
+  fragment mediaListEntry on MediaList {
+    id
+    mediaId
+    status
+    score
+    progress
+    progressVolumes
+    repeat
+    priority
+    private
+    hiddenFromStatusLists
+    customLists
+    advancedScores
+    notes
+    updatedAt
+    startedAt {
+      year
+      month
+      day
+    }
+    completedAt {
+      year
+      month
+      day
+    }
+    media {
+      id
+      title {
+        userPreferred
         romaji
         english
         native
@@ -165,214 +480,71 @@ export const DETAILS_QUERY = `
         extraLarge
         large
       }
+      type
+      format
+      status(version: 2)
+      episodes
+      volumes
+      chapters
+      averageScore
+      popularity
+      isAdult
+      countryOfOrigin
+      genres
       bannerImage
       startDate {
         year
         month
         day
       }
-      endDate {
-        year
-        month
-        day
+    }
+  }
+`
+// Viewer query requires anilist authentication.
+export const ANILIST_VIEWER_QUERY = `
+  query {
+    Viewer {
+      id
+      name
+      about
+      avatar {
+        large
       }
-      description
-      season
-      seasonYear
-      type
-      format
-      status(version: 2)
-      episodes
-      duration
-      chapters
-      volumes
-      genres
-      synonyms
-      source(version: 2)
-      isAdult
-      isLocked
-      meanScore
-      averageScore
-      popularity
-      favourites
-      hashtag
-      countryOfOrigin
-      isLicensed
-      isFavourite
-      isRecommendationBlocked
-      nextAiringEpisode {
-        airingAt
-        timeUntilAiring
-        episode
-      }
-      relations {
-        edges {
-          id
-          relationType(version: 2)
-          node {
-            id
-            title {
-              userPreferred
-            }
-            format
-            type
-            status(version: 2)
-            bannerImage
-            coverImage {
-              large
-            }
-          }
+      bannerImage
+      unreadNotificationCount
+      donatorTier
+      donatorBadge
+      moderatorRoles
+      options {
+        titleLanguage
+        staffNameLanguage
+        airingNotifications
+        displayAdultContent
+        profileColor
+        notificationOptions {
+          type
+          enabled
         }
       }
-      characterPreview: characters(perPage: 6, sort: [ROLE, RELEVANCE, ID]) {
-        edges {
-          id
-          role
-          name
-          voiceActors(language: JAPANESE, sort: [RELEVANCE, ID]) {
-            id
-            name {
-              userPreferred
-            }
-            language: languageV2
-            image {
-              large
-            }
-          }
-          node {
-            id
-            name {
-              userPreferred
-            }
-            image {
-              large
-            }
-          }
+      mediaListOptions {
+        scoreFormat
+        rowOrder
+        animeList {
+          customLists
+          sectionOrder
+          splitCompletedSectionByFormat
+          advancedScoring
+          advancedScoringEnabled
         }
-      }
-      staffPreview: staff(perPage: 8, sort: [RELEVANCE, ID]) {
-        edges {
-          id
-          role
-          node {
-            id
-            name {
-              userPreferred
-            }
-            language: languageV2
-            image {
-              large
-            }
-          }
-        }
-      }
-      studios {
-        edges {
-          isMain
-          node {
-            id
-            name
-          }
-        }
-      }
-      reviewPreview: reviews(perPage: 2, sort: [RATING_DESC, ID]) {
-        pageInfo {
-          total
-        }
-        nodes {
-          id
-          summary
-          rating
-          ratingAmount
-          user {
-            id
-            name
-            avatar {
-              large
-            }
-          }
-        }
-      }
-      recommendations(perPage: 7, sort: [RATING_DESC, ID]) {
-        pageInfo {
-          total
-        }
-        nodes {
-          id
-          rating
-          userRating
-          mediaRecommendation {
-            id
-            title {
-              userPreferred
-            }
-            format
-            type
-            status(version: 2)
-            bannerImage
-            coverImage {
-              large
-            }
-          }
-          user {
-            id
-            name
-            avatar {
-              large
-            }
-          }
-        }
-      }
-      externalLinks {
-        site
-        url
-      }
-      streamingEpisodes {
-        site
-        title
-        thumbnail
-        url
-      }
-      trailer {
-        id
-        site
-      }
-      rankings {
-        id
-        rank
-        type
-        format
-        year
-        season
-        allTime
-        context
-      }
-      tags {
-        id
-        name
-        description
-        rank
-        isMediaSpoiler
-        isGeneralSpoiler
-      }
-      mediaListEntry {
-        id
-        status
-        score
-      }
-      stats {
-        statusDistribution {
-          status
-          amount
-        }
-        scoreDistribution {
-          score
-          amount
+        mangaList {
+          customLists
+          sectionOrder
+          splitCompletedSectionByFormat
+          advancedScoring
+          advancedScoringEnabled
         }
       }
     }
   }
-`;
-
-
+`
 
