71e4f4a4291880c3b401c8caf47a31c47402e052
Mitchell Hollander <MitchellSHollander@gmail.com>
2021-12-29 19:59:51 -0600

    added callback page...still needs some work

--- src/Callback.tsx
+++ src/Callback.tsx
@@ -1,61 +1,14 @@
-import { api } from 'api';
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { ANILIST_VIEWER_QUERY, ANILIST_USER_MEDIA_LIST_COLLECTION_QUERY } from 'utils';
+import { useLocation, useHistory } from 'react-router-dom';
+import { authActions, anilistActions } from 'actions';
+import { useStateValue } from 'context';
+import Loader from 'components/Loader';
 
 function Callback() {
+  const history = useHistory();
   const location = useLocation();
   const [code, setCode] = useState('');
-
-  const getAnlistUserMediaListCollection = async (userId: number, userName: string) => {
-    const { data } = await api.fetch({
-      query: ANILIST_USER_MEDIA_LIST_COLLECTION_QUERY,
-      variables: {
-        userId,
-        userName,
-        type: 'ANIME',
-      },
-    });
-    console.log(data);
-  };
-
-  const getAnilistViewer = async (authToken: string) => {
-    const { data } = await api.fetch({
-      headers: {
-        Authorization: authToken,
-      },
-      query: ANILIST_VIEWER_QUERY,
-      variables: {},
-    });
-    console.log(data);
-    const { id, name } = data.Viewer;
-    getAnlistUserMediaListCollection(id, name);
-  };
-
-  const getAccessToken = (code: string) => {
-    // proxy stuff: https://stackoverflow.com/questions/36878255/allow-access-control-allow-origin-header-using-html5-fetch-api
-    fetch('http://localhost:8010/proxy', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        grant_type: 'authorization_code',
-        client_id: process.env.REACT_APP_ANILIST_CLIENT_ID,
-        client_secret: process.env.REACT_APP_ANILIST_CLIENT_SECRET,
-        redirect_uri: 'http://localhost:3000/callback',
-        code,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const { token_type, access_token } = data;
-        getAnilistViewer(`${token_type} ${access_token}`);
-      })
-      .catch((error) => alert(error.message));
-  };
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     if (location.pathname === '/callback') {
@@ -68,12 +21,16 @@ function Callback() {
   }, [location]);
 
   useEffect(() => {
-    if (code) {
-      getAccessToken(code);
+    if (code && user) {
+      authActions.getAccessToken(code, dispatch).then(() => {
+        anilistActions.getViewer(user.uid, dispatch).then(() => {
+          history.push('/settings');
+        });
+      });
     }
-  }, [code]);
+  }, [code, user]);
 
-  return <div className="callback">code: {code}</div>;
+  return <Loader />;
 }
 
 export default Callback;
